b0516293681e113a798248aae1ea5e5c0b80f255
ssogal00 <opengl83@gmail.com>
2015-12-07 16:50:19 +0900

    Shader 추가

---
+++ VertexShader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+
+namespace SharpOpenGL
+{
+    public class VertexShader : Shader
+    {
+
+        public VertexShader
+
+    }
+}
