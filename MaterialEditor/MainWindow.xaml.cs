98111bc5da039a110d1d0dad78edf33cff9cd2c8
Kang Il Son <opengl83@gmail.com>
2017-03-13 14:39:08 +0900

    added material editor

---
+++ MaterialEditor/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Drawing;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace MaterialEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void GLControlLoad(object sender, EventArgs e)
+        {
+            GL.ClearColor(System.Drawing.Color.Aqua);
+        }
+
+
+
+        private void GLControlPaint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            mGlControl.SwapBuffers();
+        }
+
+        private void GLControlResize(object sender, EventArgs e)
+        {
+            GL.Viewport(0, 0, mGlControl.Size.Width, mGlControl.Size.Height);
+        }
+    }
+}
