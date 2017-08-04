fdba8c5c2c7812d26306bdd8bb91f4980f2b311c
Kang Il Son <opengl83@gmail.com>
2017-08-04 17:09:24 +0900

    working on fbx mesh import

--- FBXImporter/FBXMesh.cs
+++ FBXImporter/FBXMesh.cs
@@ -9,30 +9,57 @@ using Core;
 using FBXWrapper;
 
 using SharpOpenGL;
+using Core.Buffer;
+using Core.OpenGLShader;
+using Core.Texture;
+
+using OpenTK.Graphics.OpenGL;
 
 namespace FBXImporter
 {
     public class FBXMesh
     {
         public FBXMesh(FBXWrapper.ParsedFBXMesh _ParsedFBXMesh)
-        {
-            
+        {                        
+            for(int i = 0; i < _ParsedFBXMesh.VertexList.Count; ++i)
+            {
+                SharpOpenGL.BasicMaterial.VertexAttribute NewVertex = new SharpOpenGL.BasicMaterial.VertexAttribute();
+                NewVertex.VertexPosition = _ParsedFBXMesh.VertexList[i];
+                NewVertex.TexCoord = _ParsedFBXMesh.UVList[i];
+
+                Vertices.Add(NewVertex);
+                VertexIndices.Add((uint)VertexIndices.Count);
+            }
         }
 
         public void PrepareToDraw()
         {
             VB = new Core.Buffer.StaticVertexBuffer<SharpOpenGL.BasicMaterial.VertexAttribute>();
             IB = new Core.Buffer.IndexBuffer();
+
+            var VertexArray = Vertices.ToArray();
+            VB.Bind();
+            VB.BufferData<SharpOpenGL.BasicMaterial.VertexAttribute>(ref VertexArray);
+
+            var IndexArray = VertexIndices.ToArray();
+            IB.Bind();            
+            IB.BufferData<uint>(ref IndexArray);
         }
 
         public void Draw()
         {
+            VB.Bind();
+            IB.Bind();
 
+            SharpOpenGL.BasicMaterial.VertexAttribute.VertexAttributeBinding();
+
+            GL.DrawElements(PrimitiveType.Triangles, (int)VertexIndices.Count, DrawElementsType.UnsignedInt, 0);
         }
                 
         protected List<SharpOpenGL.BasicMaterial.VertexAttribute> Vertices = new List<SharpOpenGL.BasicMaterial.VertexAttribute>();
 
         Core.Buffer.StaticVertexBuffer<SharpOpenGL.BasicMaterial.VertexAttribute> VB = null;
         Core.Buffer.IndexBuffer IB = null;
+        List<uint> VertexIndices = new List<uint>();
     }
 }
--- FBXImporter/MainWindow.xaml.cs
+++ FBXImporter/MainWindow.xaml.cs
@@ -17,9 +17,11 @@ using System.Drawing;
 using FBXWrapper;
 
 using SharpOpenGL;
-
+using Core.Buffer;
+using Core.Camera;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
+using SharpOpenGL.BasicMaterial;
 
 namespace FBXImporter
 {
@@ -35,7 +37,8 @@ namespace FBXImporter
 
             if (SdkWrapper.InitializeSDK())
             {
-                ParsedFBXMesh TestMesh = SdkWrapper.ImportFBXMesh("Sample.FBX");
+                TestParsedFbxMesh = SdkWrapper.ImportFBXMesh("Sample.FBX");
+                MyFBXMesh = new FBXMesh(TestParsedFbxMesh);
             }
         }
         private void GLControlLoad(object sender, EventArgs e)
@@ -45,6 +48,17 @@ namespace FBXImporter
             GL.CullFace(CullFaceMode.Back);
             GL.FrontFace(FrontFaceDirection.Cw);
             GL.Enable(EnableCap.DepthTest);
+
+            TestMaterial = new SharpOpenGL.BasicMaterial.BasicMaterial();
+            TestMaterial.Use();
+
+            // init uniform buffer
+            TransformBuffer = new DynamicUniformBuffer();
+            ColorBuffer = new DynamicUniformBuffer();
+            if (MyFBXMesh != null)
+            {
+                MyFBXMesh.PrepareToDraw();
+            }            
         }
 
 
@@ -56,12 +70,50 @@ namespace FBXImporter
 
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
+            //ModelTransform.View = Camera.View;
+            //ModelTransform.Proj = Camera.Proj;
+            TestMaterial.SetTransformBlockData(ref ModelTransform);
+
+            if(MyFBXMesh != null)
+            {                
+                MyFBXMesh.Draw();
+            }
+            
             GlControl.SwapBuffers();
         }
 
         private void GLControlResize(object sender, EventArgs e)
         {
-            GL.Viewport(0, 0, GlControl.Size.Width, GlControl.Size.Height);            
+            GL.Viewport(0, 0, GlControl.Size.Width, GlControl.Size.Height);
+
+            float fAspectRatio = GlControl.Size.Width / (float) GlControl.Size.Height;
+
+            Camera.AspectRatio = fAspectRatio;
+            Camera.FOV = MathHelper.PiOver6;
+            Camera.Near = 1;
+            Camera.Far = 10000;
+            Camera.EyeLocation = new Vector3(5, 5, 5);
+            Camera.DestLocation = new Vector3(5, 5, 5);
+            Camera.LookAtLocation = new Vector3(0, 0, 0);
+
+            Camera.UpdateCameraDistance();
+            Camera.UpdateProjMatrix();
+
+            ModelTransform.Proj = Matrix4.CreatePerspectiveFieldOfView(Camera.FOV, fAspectRatio, Camera.Near, Camera.Far);
+            ModelTransform.Model = Matrix4.CreateScale(0.03f);
+            ModelTransform.View = Matrix4.LookAt(new Vector3(10, 0, 0), new Vector3(0, 0, 0), Vector3.UnitY);
+                        
         }
-    }
+
+        protected SharpOpenGL.BasicMaterial.BasicMaterial TestMaterial = null;
+        protected DynamicUniformBuffer TransformBuffer = null;
+        protected DynamicUniformBuffer ColorBuffer = null;
+        protected Matrix4 ModelView = new Matrix4();
+        protected Matrix4 Projection = new Matrix4();
+        protected OrbitCamera Camera = new OrbitCamera();
+        protected SharpOpenGL.BasicMaterial.Transform ModelTransform = new SharpOpenGL.BasicMaterial.Transform();
+
+        protected ParsedFBXMesh TestParsedFbxMesh = null;
+        protected FBXMesh MyFBXMesh = null;
+    }    
 }
