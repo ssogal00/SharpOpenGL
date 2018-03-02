f3a89d3f9b75eb2487070a4a815d80f3a51f1ca0
Kang Il Son <opengl83@gmail.com>
2018-03-02 19:07:31 +0900

    work..

--- Core/Texture/RenderTargetTexture.cs
+++ Core/Texture/RenderTargetTexture.cs
@@ -6,22 +6,47 @@ using System.Threading.Tasks;
 
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
+using Core.Buffer;
+using System.Diagnostics;
 
 namespace Core.Texture
 {
-    public class RenderTargetTexture : TextureBase
+    public class RenderTargetTexture
     {
         public RenderTargetTexture(int width, int height)
             :base()
         {
-            m_Width = width;
-            m_Height = height;
-
-            m_FrameBufferObject = GL.GenFramebuffer();
-
-            
+            BufferWidth = width;
+            BufferHeight = height;
         }
 
-        int m_FrameBufferObject = 0;
+        public void Create()
+        {
+            FrameBufferObject = new FrameBuffer();
+            FrameBufferObject.Bind();
+
+            ColorAttachment = new ColorAttachmentTexture(BufferWidth, BufferHeight);
+            ColorAttachment.Resize(BufferWidth, BufferHeight);            
+
+            DepthAttachment = new DepthTargetTexture(BufferWidth, BufferHeight);
+            DepthAttachment.Resize(BufferWidth, BufferHeight);
+            
+            GL.FramebufferTexture2D(FramebufferTarget.DrawFramebuffer, FramebufferAttachment.ColorAttachment0, TextureTarget.Texture2D, ColorAttachment.GetTextureObject, 0);            
+            GL.FramebufferTexture2D(FramebufferTarget.DrawFramebuffer, FramebufferAttachment.DepthStencilAttachment, TextureTarget.Texture2D, DepthAttachment.GetTextureObject, 0);
+
+            var status = GL.CheckFramebufferStatus(FramebufferTarget.Framebuffer);
+
+            Debug.Assert(status == FramebufferErrorCode.FramebufferComplete);
+
+            FrameBufferObject.Unbind();
+        }
+
+        protected ColorAttachmentTexture ColorAttachment = null;        
+        protected DepthTargetTexture DepthAttachment = null;
+
+        protected Core.Buffer.FrameBuffer FrameBufferObject = null;
+
+        protected int BufferWidth = 0;
+        protected int BufferHeight = 0;
     }
 }
