d17c4acfb44d4e1ace4fd9d594bc15f9400f1dfd
Kang Il Son <opengl83@gmail.com>
2018-04-04 11:33:00 +0900

    working...

--- MaterialEditor/UniformParam/Vector3UniformVariableNode.cs
+++ MaterialEditor/UniformParam/Vector3UniformVariableNode.cs
@@ -8,6 +8,35 @@ namespace MaterialEditor
 {
     public class Vector3UniformVariableNode : NodeViewModel
     {
+        static int vec3UniformCount = 0;
+
+        public Vector3UniformVariableNode()
+            : base("Uniform Vector3")
+        {
+            UniformName = string.Format("uniform_vec3_{0}", vec3UniformCount++);
+        }
+
+        protected override void CreateInputOutputConnectors()
+        {
+            base.CreateInputOutputConnectors();
+
+            OutputConnectors.Add(new ConnectorViewModel("Out", ConnectorDataType.ConstantVector3, 0));
+        }
+
+        public string UniformName
+        {
+            get;set;
+        }
+
+        public override string GetExpressionForOutput(int outputIndex)
+        {
+            if(outputIndex == 0)
+            {
+                return UniformName;
+            }
+
+            return string.Empty;
+        }
 
     }
 }
--- MaterialEditor/UniformParam/Vector4UniformVariableNode.cs
+++ MaterialEditor/UniformParam/Vector4UniformVariableNode.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace MaterialEditor.UniformParam
 {
-    public class Vector4UniformVariableNode
+    public class Vector4UniformVariableNode : NodeViewModel
     {
+        static int vec3UniformCount = 0;
+
+        public Vector4UniformVariableNode()
+            : base("Uniform Vector4")
+        {
+            UniformName = string.Format("uniform_vec4_{0}", vec3UniformCount++);
+        }
+
+        protected override void CreateInputOutputConnectors()
+        {
+            base.CreateInputOutputConnectors();
+
+            OutputConnectors.Add(new ConnectorViewModel("Out", ConnectorDataType.ConstantVector4, 0));
+        }
+
+        public string UniformName
+        {
+            get; set;
+        }
+
+        public override string GetExpressionForOutput(int outputIndex)
+        {
+            if (outputIndex == 0)
+            {
+                return UniformName;
+            }
+
+            return string.Empty;
+        }
     }
 }
