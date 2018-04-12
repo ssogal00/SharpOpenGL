b0f1b09ebd6d95f7c50056503fb27ae8e4075fc8
Kang Il Son <opengl83@gmail.com>
2018-04-12 15:23:28 +0900

    working.

---
+++ Core/OpenGLContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace Core
+{
+    public class OpenGLContext
+    {
+        public OpenGLContext(OpenTK.GameWindow window)
+        {
+            this.window = window;
+        }
+
+        private OpenTK.GameWindow window = null;
+
+        public void MakeCurrent()
+        {
+            window.MakeCurrent();
+        }
+    }
+}
