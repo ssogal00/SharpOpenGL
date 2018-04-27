c4bdf9125d023db1740ab1b393fe565ce9021281
Kang Il Son <opengl83@gmail.com>
2018-04-27 16:34:05 +0900

    ...

--- Core/OpenGLContext.cs
+++ Core/OpenGLContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenTK;
+using OpenTK.Graphics.OpenGL;
 
 namespace Core
 {
@@ -12,7 +13,7 @@ namespace Core
         public OpenGLContext(OpenTK.GameWindow window)
         {
             this.window = window;
-        }        
+        }
 
         private OpenTK.GameWindow window = null;
 
@@ -21,6 +22,35 @@ namespace Core
             window.MakeCurrent();
         }
 
+        public int GetActiveTexture()
+        {
+            return GL.GetInteger(GetPName.ActiveTexture);
+        }
+
+        public int GetMaxElementsIndices()
+        {
+            return GL.GetInteger(GetPName.MaxElementsIndices);
+        }
+
+        public int GetMaxElementsVertices()
+        {
+            return GL.GetInteger(GetPName.MaxElementsVertices);
+        }
+
+        public bool IsDepthTestEnabled()
+        {            
+            return GL.GetBoolean(GetPName.DepthTest);
+        }
+
+        public int GetMaxFragmentUniformBlocks()
+        {            
+            return GL.GetInteger(GetPName.MaxFragmentUniformBlocks);
+        }
+
+        public int GetMaxTextureUnits()
+        {            
+            return GL.GetInteger(GetPName.MaxTextureUnits);
+        }
 
     }
 }
