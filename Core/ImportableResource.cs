13fe1c4dda2daa17cd59c78514642f2417ad6f8f
Kang Il Son <opengl83@gmail.com>
2018-04-30 17:51:21 +0900

    test

---
+++ Core/ImportableResource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class ImportableResource
+    {
+        public virtual async void Import() { }
+    }
+}
