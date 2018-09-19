33dfe74efeb2df09fcc291ec36a5ee0e3808391c
Kang Il Son <opengl83@gmail.com>
2018-09-19 18:49:18 +0900

    working...

--- Core/Texture/ScopedFreeImage.cs
+++ Core/Texture/ScopedFreeImage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FreeImageAPI;
 
 namespace Core.Texture
 {
@@ -10,11 +11,15 @@ namespace Core.Texture
     {
         public ScopedFreeImage(string imagePath)
         {
-
+            FreeImageHelper.Load(imagePath, out Width, out Height);
         }
         public void Dispose()
         {
-
+            FreeImage.Unload(bitmap);
         }
+
+        public FIBITMAP bitmap;
+        public int Width = 0;
+        public int Height = 0;
     }
 }
