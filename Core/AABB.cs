8e86465ad1dae45a1ef8d67524109d39055abf60
Kang Il Son <opengl83@gmail.com>
2018-10-01 15:55:18 +0900

    working...

--- Core/AABB.cs
+++ Core/AABB.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
 
 namespace Core
 {
     // Axis Aligned Bounding Box
     public class AABB
     {
+        public AABB() { }
+
+        public Vector3 Min;
+
+        public Vector3 Max;
+
+        public void Draw()
+        {
+
+        }
     }
 }
