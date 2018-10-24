88fc2aa969722b5e095c982389f35b495d5d0463
Kang Il Son <opengl83@gmail.com>
2018-10-24 11:40:44 +0900

    font rendering

---
+++ SharpOpenGL/Font/GlyphBuilder.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Numerics;
+
+namespace SharpOpenGL.Font
+{
+    /// <summary>
+    /// rendering surface that Fonts can use to generate Shapes.
+    /// </summary>
+    internal class GlyphBuilder : BaseGlyphBuilder
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GlyphBuilder"/> class.
+        /// </summary>
+        public GlyphBuilder()
+            : this(Vector2.Zero)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GlyphBuilder"/> class.
+        /// </summary>
+        /// <param name="origin">The origin.</param>
+        public GlyphBuilder(Vector2 origin)
+            : base()
+        {
+            this.builder.SetOrigin(origin);
+        }
+    }
+}
