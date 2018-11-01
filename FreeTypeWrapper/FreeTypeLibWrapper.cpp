873ac999629f3b73431fd3b51008bc05012221c8
Kang Il Son <opengl83@gmail.com>
2018-11-01 14:52:18 +0900

    turn off conformance mode

---
+++ FreeTypeWrapper/FreeTypeLibWrapper.cpp
@@ -0,0 +1,11 @@
+#include "FreeTypeLibWrapper.h"
+
+#include <msclr/marshal_cppstd.h>
+
+using namespace msclr::interop;
+
+#using <mscorlib.dll>
+
+
+void FreeTypeLibWrapper::FreeType::Initialize(System::String^ filePath)
+{}
---
+++ FreeTypeWrapper/FreeTypeLibWrapper.h
@@ -0,0 +1,12 @@
+#pragma once
+
+using namespace System::Collections::Generic;
+
+namespace FreeTypeLibWrapper
+{
+	public ref class FreeType
+	{
+	public:
+		static void Initialize(System::String^ filePath);
+	};
+}
