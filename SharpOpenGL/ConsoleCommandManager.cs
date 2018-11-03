2ca3ed29c0217591862f79405fe2719d97a02544
Kang Il Son <opengl83@gmail.com>
2018-11-03 17:23:10 +0900

    working on consolecommand

---
+++ SharpOpenGL/ConsoleCommandManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+using OpenTK;
+using OpenTK.Input;
+
+namespace SharpOpenGL
+{
+    internal class ConsoleCommandManager : Singleton<ConsoleCommandManager>
+    {
+        public void ToggleActive()
+        {
+            IsActive = !IsActive;
+        }
+
+        public void OnKeyDown(OpenTK.Input.KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.BackSpace)
+            {
+
+            }
+            else if (e.Key == Key.Enter)
+            {
+
+            }
+            else if (e.Key == Key.CapsLock)
+            {
+
+            }
+            else if (e.Key == Key.Space)
+            {
+
+            }
+        }
+
+
+        public string ConsoleCommandString = "";
+
+        public bool IsActive = false;
+
+        protected bool CapsLock = false;
+
+
+    }
+}
