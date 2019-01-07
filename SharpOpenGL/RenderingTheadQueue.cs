4833ecaec697a7f0d943c17cb0e5b9446434cdbf
Kang Il Son <opengl83@gmail.com>
2019-01-07 14:45:29 +0900

    rendering thread

---
+++ SharpOpenGL/RenderingTheadQueue.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using Core;
+
+namespace SharpOpenGL
+{
+    public class RenderingTheadQueue : Singleton<RenderingTheadQueue>
+    {
+        public void Enqueue(ThreadJob newJob)
+        {
+            JobQueue.Enqueue(newJob);
+        }
+
+        public void Execute()
+        {
+            while (JobQueue.IsEmpty == false)
+            {
+                ThreadJob job;
+                JobQueue.TryDequeue(out job);
+                job.Do();
+            }
+        }
+
+        public void ExecuteTimeSlice(double milliseconds = 100)
+        {
+            var stopwatch = new Stopwatch();
+
+            double totalElapsed = 0;
+
+            while (totalElapsed < milliseconds && JobQueue.IsEmpty == false)
+            {
+                ThreadJob job;
+                JobQueue.TryDequeue(out job);
+
+                stopwatch.Start();
+                job.Do();
+                stopwatch.Stop();
+
+                var elapsed = stopwatch.ElapsedMilliseconds;
+                totalElapsed += elapsed;
+            }
+            
+            //
+            //
+        }
+
+        ConcurrentQueue<ThreadJob> JobQueue = new ConcurrentQueue<ThreadJob>();
+    }
+}
---
+++ SharpOpenGL/ThreadJob.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpOpenGL
+{
+    public class ThreadJob
+    {
+        public ThreadJob()
+        {
+        }
+
+        public virtual void Do()
+        { }
+    }
+
+    public class TextureLoadJob : ThreadJob
+    {
+        public TextureLoadJob(string path)
+        {
+            texturePath = path;
+        }
+        public virtual void Do()
+        {
+
+        }
+
+        private string texturePath = "";
+    }
+}
