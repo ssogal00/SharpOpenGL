984f04213655b1f0ae191b4b343478d7928bcdf9
Kang Il Son <opengl83@gmail.com>
2019-01-11 18:40:41 +0900

    draw command

---
+++ Core/DrawCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class DrawCommand
+    {
+        //
+        public virtual void Setup()
+        {
+            // 
+
+            // Vertex buffer, index buffer setup
+
+            // Material Bind
+
+            // Material Parameter Setup
+
+            // 
+
+        }
+    }
+}
