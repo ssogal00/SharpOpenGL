25da78218a469fe963ca719f3ba90d0d17ad7f34
Kang Il Son <opengl83@gmail.com>
2019-01-14 11:42:12 +0900

    Update UniformParameter.cs

--- Core/UniformParameter.cs
+++ Core/UniformParameter.cs
@@ -3,13 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL;
 
 namespace Core
 {
-    public class UniformParameter
+    public class UniformParameterSet
     {
-        protected string ParamterName = "";
+        protected Dictionary<string, UniformVariableParameter> ParamSet = new Dictionary<string, UniformVariableParameter>();
+
+        public void SetParameters()
+        {
+            foreach (var param in ParamSet.Values)
+            {
+                param.SetParameter();
+            }
+        }
     }
 
+    public class UniformVariableParameter 
+    {
+        public UniformVariableParameter(string name, int location)
+        {
+            ParamterName = name;
+            Location = location;
+        }
 
+        protected string ParamterName = "";
+        protected int Location = -1;
+
+        public virtual void SetParameter()
+        {
+        }
+    }
+
+    public class UniformVariableFloatParameter : UniformVariableParameter
+    {
+        public UniformVariableFloatParameter(string name, int location, float value)
+        : base(name, location)
+        {
+            FloatValue = value;
+        }
+
+        protected float FloatValue = 0;
+
+        public override void SetParameter()
+        {
+            GL.Uniform1(Location, FloatValue);
+        }
+    }
 }
