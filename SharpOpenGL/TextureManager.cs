a51b0018cc4fadda5100b1dc78df2f155834ca58
Kang Il Son <opengl83@gmail.com>
2019-01-25 16:57:24 +0900

    Create TextureManager.cs

---
+++ SharpOpenGL/TextureManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+using Core.Texture;
+
+namespace SharpOpenGL
+{
+    public class TextureManager : Singleton<TextureManager>
+    {
+        public void ImportTextures()
+        {
+
+        }
+
+
+
+        Dictionary<string, TextureBase> TextureMap = new Dictionary<string, TextureBase>();
+    }
+}
