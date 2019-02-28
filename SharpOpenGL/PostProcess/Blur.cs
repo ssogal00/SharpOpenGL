b462fc9ad7806c4714ffc7493643b2fa8f22ee70
Kang Il Son <opengl83@gmail.com>
2019-02-28 17:58:37 +0900

    Update Blur.cs

--- SharpOpenGL/PostProcess/Blur.cs
+++ SharpOpenGL/PostProcess/Blur.cs
@@ -23,7 +23,6 @@ namespace SharpOpenGL.PostProcess
 
         public void OnResize(object sender, ScreenResizeEventArgs e)
         {
-            UpdateOffsetAndWeight(e.Width, e.Height);
         }
 
         public override void OnGLContextCreated(object sender, EventArgs e)
@@ -51,36 +50,5 @@ namespace SharpOpenGL.PostProcess
 
         protected List<OpenTK.Vector2> offset = new List<Vector2>();
         protected List<OpenTK.Vector2> weight = new List<Vector2>();
-
-        protected void UpdateOffsetAndWeight(float Width, float Height)
-        {
-            var vOffset = new List<OpenTK.Vector2>();
-            var vWeight = new List<OpenTK.Vector2>();
-
-            for (int i = 0; i < 9; i++)
-            {
-                // Compute the offsets. We take 9 samples - 4 either side and one in the middle:
-                //     i =  0,  1,  2,  3, 4,  5,  6,  7,  8
-                //Offset = -4, -3, -2, -1, 0, +1, +2, +3, +4
-                float fOffsetX = ((float)(i) - 4.0f) * (1.0f / Width);
-                float fOffsetY = ((float)(i) - 4.0f) * (1.0f / Height);
-
-                vOffset.Add(new OpenTK.Vector2(fOffsetX, fOffsetY));
-
-                // 'x' is just a simple alias to map the [0,8] range down to a [-1,+1]
-                float x = ((float)(i) - 4.0f) / 4.0f;
-
-                // Use a gaussian distribution. Changing the standard-deviation
-                // (second parameter) as well as the amplitude (multiplier) gives
-                // distinctly different results.
-
-                double fWeight = 0.5 * MathHelper.ComputeGaussianValue(x, 0.0f, 0.8f);
-                Debug.Assert(fWeight > 0.0f);
-                vWeight.Add(new OpenTK.Vector2((float)fWeight, 0));
-            }
-
-            offset = vOffset;
-            weight = vWeight;
-        }
     }
 }
