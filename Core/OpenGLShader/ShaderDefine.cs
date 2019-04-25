1ce16e4c82a186138db08e71bfbbbf1c62fd91ea
Kang Il Son <opengl83@gmail.com>
2019-04-25 12:37:35 +0900

    update

---
+++ Core/OpenGLShader/ShaderDefine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.OpenGLShader
+{
+    public class ShaderDefine
+    {
+        public ShaderDefine(string name, string value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("#define {0} {1}", Name, Value);
+        }
+
+        protected string Name;
+        protected string Value;
+    }
+}
