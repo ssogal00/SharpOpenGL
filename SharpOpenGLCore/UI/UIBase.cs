f475c8df8c7f58b8ba17164b6ecffa46341b83df
Kang Il Son <opengl83@gmail.com>
2021-03-15 00:03:13 +0900

    Update UIBase.cs

--- SharpOpenGLCore/UI/UIBase.cs
+++ SharpOpenGLCore/UI/UIBase.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
-using OpenTK.Mathematics;
+using Vector2 = OpenTK.Mathematics.Vector2;
 
 namespace SharpOpenGLCore
 {
@@ -11,9 +12,39 @@ namespace SharpOpenGLCore
         {
             Position = position;
         }
+
+        public UIVertex(Vector2 position, Vector2 texcoord)
+        {
+            Position = position;
+            Texcoord = texcoord;
+            Color = Vector4.One;
+        }
+
+        public UIVertex(Vector2 position, Vector2 texcoord, Vector4 color)
+        {
+            Position = position;
+            Texcoord = texcoord;
+            Color = color;
+        }
+
+        
+
         public Vector2 Position;
         public Vector2 Texcoord;
-        public float Alpha;
+        public Vector4 Color; // including alpha
+    }
+
+    public enum EAnchorPosition
+    {
+        LeftTop,
+        LeftCenter,
+        LeftBottom,
+        CenterTop,
+        CenterCenter,
+        CenterBottom,
+        RightTop,
+        RightCenter,
+        RightBottom,
     }
 
     public class UIBase
@@ -26,28 +57,67 @@ namespace SharpOpenGLCore
         protected virtual void BuildVertexList()
         {
             mCachedVertexList.Clear();
-            
-            var leftTop = new UIVertex(mLeftTop);
-            var rifhtTop = new UIVertex(RightTop);
 
-            
+            Vector2 anchoredPos = GetAnchorPosition(mAnchorPos);
+
+            var leftTop = new UIVertex(anchoredPos+ mLeftTop, Vector2.Zero);
+            var rightTop = new UIVertex(anchoredPos+RightTop, new Vector2(1,0));
+            var leftBtm = new UIVertex(anchoredPos+ LeftBottom, new Vector2(0,1));
+            var rightBtm = new UIVertex(anchoredPos + mRightBottom, new Vector2(1,1));
+
+            mCachedVertexList.Add(leftTop);
+            mCachedVertexList.Add(rightTop);
+            mCachedVertexList.Add(leftBtm);
+
+            mCachedVertexList.Add(leftBtm);
+            mCachedVertexList.Add(rightTop);
+            mCachedVertexList.Add(rightBtm);
+        }
+
+
+        public Vector2 GetAnchorPosition(EAnchorPosition anchorType)
+        {
+            switch (anchorType)
+            {
+
+            }
+
+            return Vector2.Zero;
+        }
+        
+
+        public List<UIVertex> GetVertexList()
+        {
+            return mCachedVertexList;
         }
 
         public Vector2 RightTop
         {
-            get
-            {
-                return new Vector2(mLeftTop.X+mWidth, mLeftTop.Y);
-            }
+            get { return new Vector2(mLeftTop.X + mWidth, mLeftTop.Y); }
         }
+
+        public Vector2 LeftBottom
+        {
+            get { return new Vector2(mLeftTop.X, mLeftTop.Y + mHeight);}
+        }
+
+        public Vector2 RightBottom
+        {
+            get { return new Vector2(mLeftTop.X + mWidth, mLeftTop.Y+ mHeight);}
+        }
+        
+
+        protected UIBase mParent = null;
 
         public Vector2 LeftTop => mLeftTop;
 
         protected Vector2 mLeftTop = Vector2.Zero;
 
-        protected Vector2 mRightBottm;
+        protected Vector2 mRightBottom;
 
         protected Vector2 mAnchor;
+
+        protected EAnchorPosition mAnchorPos = EAnchorPosition.LeftTop;
 
         protected int mWidth;
 
@@ -63,11 +133,19 @@ namespace SharpOpenGLCore
     public class UIText : UIBase
     {
         public string Text = string.Empty;
+        
     }
 
-    // 
-    public class Screen2D
+    // top most ui 
+    public class UIScreen : UIBase
     {
+        public int Width = 1920;
+        public int Height = 1080;
 
+        protected List<UIBase> mChildList=new List<UIBase>();
+
+        
     }
+
+
 }
